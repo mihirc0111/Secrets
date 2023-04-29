68eb6d10dad999be70251cca396e2eda9f3c1122
MIHIR CHAVAN <mihirc0111@gmail.com>
2023-04-29 17:27:29 +0530

    Level2- Encryption

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
- 
+const encrypt = require("mongoose-encryption");
 const app = express();
  
 app.use(express.static("public"));
@@ -31,11 +31,22 @@ async function main() {
 } 
 
 
-const userSchema = {
+
+
+
+
+const userSchema = new mongoose.Schema ({
     email: String,
     password: String
-};
+  });
  
+  const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+
+
+
+
+
 const User = new mongoose.model("User", userSchema);
  
  
