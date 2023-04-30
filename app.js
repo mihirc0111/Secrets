1f1fb55645abd23e9a948fa9e7288c116baaf861
MIHIR CHAVAN <mihirc0111@gmail.com>
2023-04-30 10:39:14 +0530

    Updated callback url,last commit has an extra /

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     // callbackURL: "http://localhost:3000/auth/google/secrets",
-    callbackURL: "https://mihirs-secrets-sharing-website.onrender.com//auth/google/secrets",
+    callbackURL: "https://mihirs-secrets-sharing-website.onrender.com/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
